6c24cb79d63294495cabfbd39b0524ff67bd3569
Otamio <ca.john.wang@gmail.com>
2021-12-22 18:36:15 -0800

    include entries in validation and test set

--- codes/create_mapping.py
+++ codes/create_mapping.py
@@ -6,12 +6,15 @@ if __name__ == "__main__":
     entities = set()
     relations = set()
 
-    with open(f"{dataset_path}/train.txt") as fd:
-        for line in fd:
-            s, r, o = [x.strip() for x in line.split()]
-            entities.add(s)
-            relations.add(r)
-            entities.add(o)
+    for fname in [f"{dataset_path}/train.txt",
+                  f"{dataset_path}/valid.txt",
+                  f"{dataset_path}/test.txt"]:
+        with open(fname) as fd:
+            for line in fd:
+                s, r, o = [x.strip() for x in line.split()]
+                entities.add(s)
+                relations.add(r)
+                entities.add(o)
 
     with open(f"{dataset_path}/entities.dict", 'w') as fd:
         for i, entity in enumerate(entities):
