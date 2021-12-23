4193098969a6766063bd1ec0d6673a2b6ab34667
Otamio <ca.john.wang@gmail.com>
2021-12-23 00:14:53 -0800

    Evaluate Test set when do validation

--- codes/run.py
+++ codes/run.py
@@ -337,9 +337,15 @@ def main(args):
                 training_logs = []
 
             if args.do_valid and step % args.valid_steps == 0:
+
                 logging.info('Evaluating on Valid Dataset...')
                 metrics = kge_model.test_step(kge_model, valid_triples, all_true_triples, args)
                 log_metrics('Valid', step, metrics)
+
+                if args.do_test:
+                    logging.info('Evaluating on Test Dataset...')
+                    metrics = kge_model.test_step(kge_model, test_triples, all_true_triples, args)
+                    log_metrics('Test', step, metrics)
 
         save_variable_list = {
             'step': step,
