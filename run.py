c4eb32d53e7e18573337735c8a955116d0e04486
Otamio <ca.john.wang@gmail.com>
2021-12-23 10:50:02 -0800

    fix long string issue

--- run.py
+++ run.py
@@ -6,12 +6,15 @@ if __name__ == "__main__":
     model = sys.argv[1].lower()
     dataset = sys.argv[2].lower()
     if model == "rotate":
-        subprocess.run(f'''python codes/create_mapping.py data/{dataset} && 
-        CUDA_VISIBLE_DEVICES=0 python -u codes/run.py --do_train --cuda --do_valid --do_test 
-        --data_path data/{dataset} --model {model} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv -lr 0.0001 
-        --max_steps 150000 --valid_steps 500 -save out/{dataset}_{model} --test_batch_size 16 -de''', shell=True)
+        subprocess.run(f"python codes/create_mapping.py data/{dataset} && " \
+                       "CUDA_VISIBLE_DEVICES=0 python -u codes/run.py --do_train --cuda --do_valid --do_test " \
+                       f"--data_path data/{dataset} --model {model} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
+                       f"-lr 0.0001 --max_steps 150000 --valid_steps 500 -save out/{dataset}_{model} " \
+                       "--test_batch_size 16 -de",
+                       shell=True)
     elif model == "transe":
-        subprocess.run(f'''python codes/create_mapping.py data/{dataset} && 
-        CUDA_VISIBLE_DEVICES=0 python -u codes/run.py --do_train --cuda --do_valid --do_test 
-        --data_path data/{dataset} --model {model} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv -lr 0.0001 
-        --max_steps 150000 --valid_steps 500 -save out/{dataset}_{model} --test_batch_size 16''', shell=True)
+        subprocess.run(f"python codes/create_mapping.py data/{dataset} && " \
+                       "CUDA_VISIBLE_DEVICES=0 python -u codes/run.py --do_train --cuda --do_valid --do_test " \
+                       f"--data_path data/{dataset} --model {model} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
+                       f"-lr 0.0001 --max_steps 150000 --valid_steps 500 -save out/{dataset}_{model} " \
+                       "--test_batch_size 16", shell=True)
