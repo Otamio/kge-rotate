2cc57b06927f17ec0a3a3e8f3ff78f68cd86980b
Otamio <ca.john.wang@gmail.com>
2021-12-23 11:27:58 -0800

    update evaluation

--- run.py
+++ run.py
@@ -15,13 +15,13 @@ if __name__ == "__main__":
         subprocess.run(f"python codes/create_mapping.py data/{dataset} && " \
                        f"CUDA_VISIBLE_DEVICES={gpu} python -u codes/run.py --do_train --cuda --do_valid --do_test " \
                        f"--data_path data/{dataset} --model {mapping[model]} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
-                       f"-lr 0.0001 --max_steps 150000 --valid_steps 1500 -save out/{dataset}_{model} " \
+                       f"-lr 0.0001 --max_steps 150000 --valid_steps 5000 -save out/{dataset}_{model} " \
                        "--test_batch_size 16 -de",
                        shell=True)
     elif model == "transe":
         subprocess.run(f"python codes/create_mapping.py data/{dataset} && " \
                        f"CUDA_VISIBLE_DEVICES={gpu} python -u codes/run.py --do_train --cuda --do_valid --do_test " \
                        f"--data_path data/{dataset} --model {mapping[model]} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
-                       f"-lr 0.0001 --max_steps 150000 --valid_steps 1500 -save out/{dataset}_{model} " \
+                       f"-lr 0.0001 --max_steps 150000 --valid_steps 5000 -save out/{dataset}_{model} " \
                        "--test_batch_size 16", shell=True)
 
