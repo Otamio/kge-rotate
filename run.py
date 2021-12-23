0470435c64c2d50b34ef94a7263c759e91985f79
Otamio <ca.john.wang@gmail.com>
2021-12-23 10:58:37 -0800

    add gpu

--- run.py
+++ run.py
@@ -10,16 +10,17 @@ mapping = {
 if __name__ == "__main__":
     model = sys.argv[1].lower()
     dataset = sys.argv[2]
+    gpu = sys.argv[3]
     if model == "rotate":
         subprocess.run(f"python codes/create_mapping.py data/{dataset} && " \
-                       "CUDA_VISIBLE_DEVICES=0 python -u codes/run.py --do_train --cuda --do_valid --do_test " \
+                       f"CUDA_VISIBLE_DEVICES={gpu} python -u codes/run.py --do_train --cuda --do_valid --do_test " \
                        f"--data_path data/{dataset} --model {mapping[model]} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
                        f"-lr 0.0001 --max_steps 150000 --valid_steps 500 -save out/{dataset}_{model} " \
                        "--test_batch_size 16 -de",
                        shell=True)
     elif model == "transe":
         subprocess.run(f"python codes/create_mapping.py data/{dataset} && " \
-                       "CUDA_VISIBLE_DEVICES=0 python -u codes/run.py --do_train --cuda --do_valid --do_test " \
+                       f"CUDA_VISIBLE_DEVICES={gpu} python -u codes/run.py --do_train --cuda --do_valid --do_test " \
                        f"--data_path data/{dataset} --model {mapping[model]} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
                        f"-lr 0.0001 --max_steps 150000 --valid_steps 500 -save out/{dataset}_{model} " \
                        "--test_batch_size 16", shell=True)
