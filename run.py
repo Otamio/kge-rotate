d7bd5fd6bb7e03f005648df0d6709617ac96e950
Otamio <ca.john.wang@gmail.com>
2021-12-23 10:52:17 -0800

    fix lower case issue

--- run.py
+++ run.py
@@ -2,19 +2,24 @@ import sys
 import subprocess
 
 
+mapping = {
+    "rotate": "RotatE",
+    "transe": "TransE"
+}
+
 if __name__ == "__main__":
-    model = sys.argv[1]
+    model = sys.argv[1].lower()
     dataset = sys.argv[2].lower()
-    if model.lower() == "rotate":
+    if model == "rotate":
         subprocess.run(f"python codes/create_mapping.py data/{dataset} && " \
                        "CUDA_VISIBLE_DEVICES=0 python -u codes/run.py --do_train --cuda --do_valid --do_test " \
-                       f"--data_path data/{dataset} --model {model} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
+                       f"--data_path data/{dataset} --model {mapping[model]} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
                        f"-lr 0.0001 --max_steps 150000 --valid_steps 500 -save out/{dataset}_{model} " \
                        "--test_batch_size 16 -de",
                        shell=True)
-    elif model.lower() == "transe":
+    elif model == "transe":
         subprocess.run(f"python codes/create_mapping.py data/{dataset} && " \
                        "CUDA_VISIBLE_DEVICES=0 python -u codes/run.py --do_train --cuda --do_valid --do_test " \
-                       f"--data_path data/{dataset} --model {model} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
+                       f"--data_path data/{dataset} --model {mapping[model]} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
                        f"-lr 0.0001 --max_steps 150000 --valid_steps 500 -save out/{dataset}_{model} " \
                        "--test_batch_size 16", shell=True)
