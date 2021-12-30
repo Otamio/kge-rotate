e757e2f46d0ab841b6687489379fa39e4ad3e08c
Otamio <ca.john.wang@gmail.com>
2021-12-29 23:28:52 -0800

    fix path issue

--- numeric.py
+++ numeric.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
 
     command_transe = f"python codes/create_mapping.py --dataset data/{dataset} && " \
                      f"CUDA_VISIBLE_DEVICES={gpu} python -u codes/run.py --do_train --cuda --do_valid --do_test " \
-                     f"--data_path data/{dataset} --model TransE -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
-                     f"-lr 0.0001 --max_steps 150000 --valid_steps 5000 -save out/{dataset}_transe " \
+                     f"--data_path numeric/{dataset} --model TransE -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
+                     f"-lr 0.0001 --max_steps 150000 --valid_steps 5000 -save numeric/{dataset}_transe " \
                      "--test_batch_size 16 --use_stopper --save_best"
 
     command_rotate = f"python codes/create_mapping.py --dataset data/{dataset} && " \
