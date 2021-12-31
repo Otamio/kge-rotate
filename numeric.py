312b6003408dfca28b911821544a6fe2378fb1ac
Otamio <ca.john.wang@gmail.com>
2021-12-30 23:34:58 -0800

    add ablation commands

--- numeric.py
+++ numeric.py
@@ -16,18 +16,16 @@ if __name__ == "__main__":
     gpu = args.gpu
     options = args.options
 
-    command_transe = f"python codes/create_mapping.py --dataset data/{dataset} && " \
+    command_transe = f"python codes/create_mapping.py --dataset numeric/{dataset} && " \
                      f"CUDA_VISIBLE_DEVICES={gpu} python -u codes/run.py --do_train --cuda --do_valid --do_test " \
                      f"--data_path numeric/{dataset} --model TransE -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
                      f"-lr 0.0001 --max_steps 150000 --valid_steps 5000 -save numeric/{dataset}_transe " \
                      "--test_batch_size 16 --use_stopper --save_best"
 
-    command_rotate = f"python codes/create_mapping.py --dataset data/{dataset} && " \
-                     f"CUDA_VISIBLE_DEVICES={gpu} python -u codes/run.py --do_train --cuda --do_valid --do_test " \
+    command_rotate = f"CUDA_VISIBLE_DEVICES={gpu} python -u codes/run.py --do_train --cuda --do_valid --do_test " \
                      f"--data_path numeric/{dataset} --model RotatE -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
                      f"-lr 0.0001 --max_steps 150000 --valid_steps 5000 -save numeric/{dataset}_rotate " \
                      "--test_batch_size 16 -de --use_stopper --save_best"
 
-    subprocess.run(f"python codes/create_mapping.py --dataset numeric/{dataset}", shell=True)
     subprocess.run(command_transe, shell=True)
     subprocess.run(command_rotate, shell=True)
